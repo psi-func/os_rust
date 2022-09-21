655a61890365169433c8edf32543227e32ef26d6
psi-func <shalaginmx998@yandex.ru>
2022-09-21 18:15:26 +0300

    simple print

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,11 @@
 #![no_std]
 #![no_main]
 
+mod vga_buffer;
+
 use core::panic::PanicInfo;
+
+use vga_buffer::print_smth;
 
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
@@ -12,13 +16,14 @@ static HELLO: &[u8] = b"Hello World!";
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    let vga_buffer = 0xb8000 as *mut u8;
+    print_smth();
+    // let vga_buffer = 0xb8000 as *mut u8;
 
-    for (i, &byte) in HELLO.iter().enumerate() {
-        unsafe {
-            *vga_buffer.offset(i as isize * 2) = byte;
-            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
-        }
-    }
+    // for (i, &byte) in HELLO.iter().enumerate() {
+    //     unsafe {
+    //         *vga_buffer.offset(i as isize * 2) = byte;
+    //         *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
+    //     }
+    // }
     loop {}
 }
